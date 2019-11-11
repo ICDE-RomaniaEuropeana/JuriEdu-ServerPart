f4ec33c59f79327ec121a5e1f64edd94e2dc25b4
Aurel Canciu <aurelcanciu@gmail.com>
2019-11-11 22:19:50 +0200

    Fix NPE

--- src/main/java/org/icde/juriedu/search/InputObject.java
+++ src/main/java/org/icde/juriedu/search/InputObject.java
@@ -2,10 +2,12 @@ package org.icde.juriedu.search;
 
 import org.icde.juriedu.model.EntryType;
 
+import java.util.Objects;
+
 public class InputObject {
 
     private EntryType type;
-    private String search;
+    private String search = "";
     private Integer size = 50;
 
     public EntryType getType() {
@@ -13,21 +15,21 @@ public class InputObject {
     }
 
     public InputObject setType(EntryType type) {
-        this.type = type;
+        this.type = Objects.requireNonNull(type);
         return this;
     }
 
     public String getSearch() { return search; }
 
     public InputObject setSearch(String search) {
-        this.search = search;
+        Objects.requireNonNull(search);Objects.requireNonNull(search);
         return this;
     }
 
     public Integer getSize() { return size; }
 
     public InputObject setSize(Integer size) {
-        this.size = size;
+        this.size = Objects.requireNonNull(size);
         return this;
     }
 }
