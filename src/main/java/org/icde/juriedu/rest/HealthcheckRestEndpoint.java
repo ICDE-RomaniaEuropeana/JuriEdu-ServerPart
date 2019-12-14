6bb1acea346c2e0f0f66197be9d5d91880cf0221
Aurel Canciu <aurelcanciu@gmail.com>
2019-12-14 14:03:23 +0200

    Fix HC endpoint

--- src/main/java/org/icde/juriedu/rest/HealthcheckRestEndpoint.java
+++ src/main/java/org/icde/juriedu/rest/HealthcheckRestEndpoint.java
@@ -2,12 +2,14 @@ package org.icde.juriedu.rest;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
+@Path("")
 public class HealthcheckRestEndpoint {
     @GET
     @Path("/_healthcheck")
     @Produces(MediaType.APPLICATION_JSON)
-    public String healthCheck() {
-        return "ok";
+    public Response healthCheck() {
+        return Response.ok("ok").build();
     }
 }
